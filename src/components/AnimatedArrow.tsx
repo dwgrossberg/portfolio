cc4583523d26aa92702faf44ac6b21e234aee737
Daniel <dwgrossberg@gmail.com>
2022-12-09 07:40:46 -0500

    Add send message / email svg animation

---
+++ src/components/AnimatedArrow.tsx
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import theme from "../utilities/theme";
+
+const AnimatedArrow = ({
+  colorMode,
+  arrowPath,
+}: {
+  colorMode: string;
+  arrowPath: string;
+}) => {
+  const transition = {
+    duration: 0.5,
+    type: "spring",
+    damping: 20,
+    stiffness: 200,
+  };
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-labelledby="stackIconTitle"
+    >
+      <motion.path
+        fill={
+          colorMode === "dark"
+            ? theme.colors.light.text
+            : theme.colors.dark.text
+        }
+        strokeWidth="1"
+        stroke={
+          colorMode === "dark"
+            ? theme.colors.light.text
+            : theme.colors.dark.text
+        }
+        strokeLinejoin="miter"
+        strokeLinecap="square"
+        animate={{ d: arrowPath }}
+        transition={transition}
+        initial="stack"
+        whileHover="line"
+      />
+    </svg>
+  );
+};
+
+export default AnimatedArrow;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,26 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import theme from "../utilities/theme";
+import AnimatedArrow from "../components/AnimatedArrow";
 import "../styles/pages.css";
 
 const Contact = ({ colorMode }: { colorMode: string }): JSX.Element => {
+  const [arrowPath, setArrowPath] = useState(
+    "M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"
+  );
+  const transition = {
+    duration: 0.5,
+    type: "spring",
+    damping: 20,
+    stiffness: 200,
+  };
   const form = useRef<HTMLFormElement>(null);
   const sendEmail = (e: any) => {
     e.preventDefault();
     if (form.current == null) return;
-
     emailjs
       .sendForm(
         "service_a0602jv",
@@ -106,6 +115,7 @@ const Contact = ({ colorMode }: { colorMode: string }): JSX.Element => {
             css={css`
               width: 100%;
               padding: 5px;
+              margin-bottom: 10px;
               color: ${colorMode === "dark"
                 ? theme.colors.light.text
                 : theme.colors.dark.text};
@@ -114,9 +124,7 @@ const Contact = ({ colorMode }: { colorMode: string }): JSX.Element => {
                 : theme.colors.light.backgroundBall};
             `}
           />
-          <motion.input
-            type="submit"
-            value="Send"
+          <motion.div
             whileHover={{
               scale: 1.1,
               boxShadow: `0px 0px 15px 9px ${
@@ -133,29 +141,60 @@ const Contact = ({ colorMode }: { colorMode: string }): JSX.Element => {
               borderRadius: "7px",
             }}
             whileTap={{ scale: 0.9 }}
-            transition={{ duration: 0.5 }}
+            transition={{ transition }}
+            onMouseEnter={() => {
+              setArrowPath(
+                "M5 24.225V7.776h22v16.447H5v.002zm3.011-1.815h15.978l-5.111-5.115L16 20.179l-2.877-2.883-5.112 5.114zm-1.216-1.275l5.077-5.09-5.077-5.065v10.155zm13.332-5.09l5.079 5.09V10.979l-5.079 5.066zm-4.126 1.588l8.022-8.027-16.045-.001 8.023 8.028z"
+              );
+            }}
+            onMouseLeave={() => {
+              setArrowPath(
+                "M18.629 15.997l-7.083-7.081L13.462 7l8.997 8.997L13.457 25l-1.916-1.916z"
+              );
+            }}
             css={css`
-              width: 50%;
+              position: relative;
+              width: 35%;
               place-self: center;
-              margin-top: 10px;
-              padding: 5px 10px;
-              cursor: pointer;
-              color: ${colorMode === "dark"
-                ? theme.colors.light.text
-                : theme.colors.dark.text};
-              background-color: ${colorMode === "dark"
-                ? theme.colors.dark.backgroundAccent
-                : theme.colors.light.backgroundAccent};
-              box-shadow: 0px 0px 5px 3px
-                ${colorMode === "dark"
+            `}
+          >
+            <input
+              type="submit"
+              value="Send"
+              css={css`
+                width: 100%;
+                place-self: center;
+                padding: 5px 10px;
+                cursor: pointer;
+                text-align: left;
+                padding-left: 37px;
+                color: ${colorMode === "dark"
+                  ? theme.colors.light.text
+                  : theme.colors.dark.text};
+                background-color: ${colorMode === "dark"
                   ? theme.colors.dark.backgroundAccent
                   : theme.colors.light.backgroundAccent};
-              text-shadow: 1px 1px
-                ${colorMode === "dark"
-                  ? theme.colors.dark.backgroundBall
-                  : theme.colors.light.backgroundBall};
-            `}
-          />
+                box-shadow: 0px 0px 5px 3px
+                  ${colorMode === "dark"
+                    ? theme.colors.dark.backgroundAccent
+                    : theme.colors.light.backgroundAccent};
+                text-shadow: 1px 1px
+                  ${colorMode === "dark"
+                    ? theme.colors.dark.backgroundBall
+                    : theme.colors.light.backgroundBall};
+              `}
+            />
+            <div
+              css={css`
+                position: absolute;
+                width: 20px;
+                right: 27%;
+                top: 8%;
+              `}
+            >
+              <AnimatedArrow colorMode={colorMode} arrowPath={arrowPath} />
+            </div>
+          </motion.div>
         </form>
       </div>
     </div>
